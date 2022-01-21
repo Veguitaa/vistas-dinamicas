474c67eb29feabe7fe7febd38920178f8073d770
Veguitaa <webav2020@gmail.com>
2022-01-20 22:50:18 -0300

    practico pimienta y sal

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const {index, detalle} = require('../controllers/indexController')
+
+
+/* GET home page. */
+router.get('/', index);
+
+router.get('/detalle/:id', detalle)
+
+module.exports = router;
